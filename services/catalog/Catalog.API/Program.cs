26ebb8d2a35461ad0add95b7083d7a69662ed0c6
Zakaria Mostafa <zakariamostafa015@gmail.com>
2026-06-19 18:27:05 +0300

    chore: add swagger

--- services/catalog/Catalog.API/Program.cs
+++ services/catalog/Catalog.API/Program.cs
@@ -6,12 +6,19 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen(c =>
+{
+   
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
