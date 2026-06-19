bfffbe5aecbacee20f9e91c5378636e6b0aea099
Zakaria Mostafa <zakariamostafa015@gmail.com>
2026-06-19 19:00:40 +0300

    chore: project references

--- services/catalog/Catalog.API/Program.cs
+++ services/catalog/Catalog.API/Program.cs
@@ -6,9 +6,19 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddSwaggerGen(c =>
+builder.Services.AddSwaggerGen(options =>
 {
-   
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo
+    {
+        Title = "Catalog API",
+        Version = "v1",
+        Description = "Catalog Microservice using .net",
+        Contact = new Microsoft.OpenApi.OpenApiContact
+        {
+            Name = "Zakaria Mostafa",
+            Email = "zakariamostafa010@gmail.com"
+        }
+    });
 });
 
 var app = builder.Build();
