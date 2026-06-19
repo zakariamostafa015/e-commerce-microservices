a9129fef6da9ba9d4d9038889a4466016fcf115f
Zakaria Mostafa <zakariamostafa015@gmail.com>
2026-06-19 19:38:07 +0300

    feat: create catalog entites

---
+++ services/catalog/Catalog.Core/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Summary { get; set; }
+        public string ImageFile { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public ProductBrand Brand { get; set; }
+        public ProductType Type { get; set; }
+
+    }
+}
